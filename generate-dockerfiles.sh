caac73e93323e68b6aa4b9572eedb7cce467f45f
bfren <bf@bfren.dev>
2023-05-10 18:36:46 +0100

    v3.0.1 (#60)
    Minor updates
    * Using latest base images
    * Adding Alpine 3.18

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="4.4.17"
+BASE_REVISION="4.5.0"
 echo "Base: ${BASE_REVISION}"
 
-ALPINE_VERSIONS="3.8 3.10 3.13 3.14 3.15 3.16 3.17 edge"
+ALPINE_VERSIONS="3.8 3.10 3.13 3.14 3.15 3.16 3.17 3.18 edge"
 for V in ${ALPINE_VERSIONS} ; do
 
     echo "Apache for Alpine ${V}"
