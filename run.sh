14e0f6d767a177143874e0ded7e275fbe52069d8
bfren <bf@bfren.dev>
2023-09-13 09:33:43 +0000

    v3.0.8 (#67)
    Minor updates
    * Using latest base images
    
    Development updates
    * Adding --load to run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ IMAGE=`cat VERSION`
 ALPINE=${1:-3.17}
 
 docker buildx build \
+    --load \
     --build-arg BF_IMAGE=apache \
     --build-arg BF_VERSION=${IMAGE} \
     -f alpine${ALPINE}/Dockerfile \
